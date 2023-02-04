9331bdfc5dec03195dba11a105b99c28cb9c51f8
Todd <tmtappr@gmail.com>
2023-02-04 11:33:10 -0800

    grr

--- doc/linked_vector/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":[["Cursor",""],["CursorMut",""],["HNode",""],["Handles",""],["IntoIter",""],["Iter",""],["IterMut",""],["LinkedVector","A doubly-linked list that uses handles to refer to elements that exist within a vector. This allows for O(1) insertion and removal of elements from the list, and O(1) access to elements by handle."]],"trait":[["CursorBase",""]]};
